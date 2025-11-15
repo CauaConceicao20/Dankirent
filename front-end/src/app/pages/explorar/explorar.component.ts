0e9256d1ec4dd51e8eef4febaf22e7918a438a3b
Cauã Conceição <cauaconceicaodev@hotmail.com>
2025-11-15 20:45:16 -0300

    feat: add component header

---
+++ front-end/src/app/pages/explorar/explorar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-apresentation',
+  imports: [HeaderComponent],
+  templateUrl: './explorar.component.html',
+  styleUrl: './explorar.component.css'
+})
+export class ExplorarComponent {
+
+}
