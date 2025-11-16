b8dcd19099fda2b7e52cfca75df9d4fb2d84f3ad
Cauã Conceição <cauaconceicaodev@hotmail.com>
2025-11-16 14:12:09 -0300

    refactor: organize the project components

--- front-end/src/app/pages/explorar/explorar.component.ts
+++ front-end/src/app/pages/explorar/explorar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { CategoriesSectionComponent } from "./components/categories-section/categories-section.component";
 
 @Component({
   selector: 'app-apresentation',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HeroSectionComponent, CategoriesSectionComponent],
   templateUrl: './explorar.component.html',
   styleUrl: './explorar.component.css'
 })
