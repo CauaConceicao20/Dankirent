8188d0682f51372e1efab9c068737f98d840a88f
Cauã Conceição <cauaconceicaodev@hotmail.com>
2025-11-20 15:04:25 -0300

    feat: add a featured products section to the explore page

--- front-end/src/app/components/card-item/card-item.component.ts
+++ front-end/src/app/components/card-item/card-item.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card-item',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './card-item.component.html',
   styleUrl: './card-item.component.scss'
 })
 export class CardItemComponent {
-
+  @Input() id!: number;
+  @Input() title!: string;
+  @Input() category!: string;
+  @Input() state!: string;
+  @Input() imageUrl!: string;
+  @Input() description!: string;
+  @Input() address!: string;
+  @Input() uf!: string;
+  @Input() price!: number;
+  @Input() priceHour!: number;
+  @Input() rating!: number;
+  @Input() reviewsCount!: number;
+  @Input() delivery!: boolean;
 }
