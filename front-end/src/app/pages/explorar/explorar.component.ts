3c5a99afa309f5b5756b5809b9d02050470cf4a3
Cauã Conceição <cauaconceicaodev@hotmail.com>
2025-11-21 00:15:41 -0300

    feat: add a footer on the explore page

--- front-end/src/app/pages/explorar/explorar.component.ts
+++ front-end/src/app/pages/explorar/explorar.component.ts
@@ -4,10 +4,12 @@ import { HeroSectionComponent } from "./components/hero-section/hero-section.com
 import { CategoriesSectionComponent } from "./components/categories-section/categories-section.component";
 import { FeaturedItemsSectionComponent } from './components/featured-items-section/featured-items-section.component';
 import { BenefitsSectionComponent } from './components/benefits-section/benefits-section.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-apresentation',
-  imports: [HeaderComponent, HeroSectionComponent, CategoriesSectionComponent, FeaturedItemsSectionComponent,  BenefitsSectionComponent],
+  imports: [HeaderComponent, HeroSectionComponent, CategoriesSectionComponent, FeaturedItemsSectionComponent,
+      BenefitsSectionComponent, FooterComponent  ],
   templateUrl: './explorar.component.html',
   styleUrl: './explorar.component.scss'
 })
