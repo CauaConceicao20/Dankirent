91a898ce904631b6bfbc80a1270fbabc736535e9
Cauã Conceição <cauaconceicaodev@hotmail.com>
2025-11-29 13:18:53 -0300

    feat: add options menu to header

--- front-end/src/app/components/header/header.component.ts
+++ front-end/src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   @Input() optionNavSelect!: String;
 
-  select(item: string) {
+  public menuIsVisible : Boolean = false;
+
+  public select(item: string) {
     this.optionNavSelect = item;
   }
 }
