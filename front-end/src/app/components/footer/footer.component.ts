a737eac4e5cc25e08cf8afa1e5204050ae26d963
Cauã Conceição <cauaconceicaodev@hotmail.com>
2025-12-08 21:43:54 -0300

    feat: add function to activate modal

--- front-end/src/app/components/footer/footer.component.ts
+++ front-end/src/app/components/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { AuthenticationService } from '../../services/auth/authentication.service';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-footer',
@@ -8,4 +11,23 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+
+  public announcetProduct(): void {
+    if (this.authenticationService.isLogged()) {
+      this.router.navigate(['/announcet']);
+      return;
+    }
+
+    ModalComponent.open({
+      title: "Entre para continuar",
+      message: "Você precisa estar conectado para criar um anúncio. Acesse sua conta ou registre-se rapidamente.",
+      confirmText: "Entrar",
+      cancelText: "Cancelar",
+      action: () => {
+        ModalComponent.close();
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
--- front-end/src/app/pages/explorar/components/hero-section/hero-section.component.ts
+++ front-end/src/app/pages/explorar/components/hero-section/hero-section.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
+import { AuthenticationService } from '../../../../services/auth/authentication.service';
+import { ModalComponent } from '../../../../components/modal/modal.component';
 
 @Component({
   selector: 'app-hero-section',
@@ -9,4 +11,23 @@ import { RouterLink } from "@angular/router";
 })
 export class HeroSectionComponent {
 
+  constructor(private authenticationService : AuthenticationService, private router : Router) {}
+
+  public announcetProduct() : void {
+    if(this.authenticationService.isLogged()) {
+      this.router.navigate(['/announcet']);
+      return;
+    }
+
+    ModalComponent.open({
+      title: "Entre para continuar",
+      message: "Você precisa estar conectado para criar um anúncio. Acesse sua conta ou registre-se rapidamente.",
+      confirmText: "Entrar",
+      cancelText: "Cancelar",
+      action: () => {
+        ModalComponent.close();
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
